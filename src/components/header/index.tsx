6c8df08f348ec9cecc6980f3919f195a5b006d0c
Oscar <oskharm12@gmail.com>
2024-01-10 23:05:35 -0300

    iniciando criação do Header

---
+++ src/components/header/index.tsx
@@ -0,0 +1,14 @@
+import styles from "./styles.module.css"
+const Header =()=>{
+    return(
+        <header>
+            <section>
+                <nav>
+                    <h1>Tarefas</h1>
+                </nav>
+            </section>
+        </header>
+    )
+}
+
+export default Header
