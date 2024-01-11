2c6f36265567b08e76f78acceb566bc426028b92
Dikma Digital2 <dikma.digital2@grupodikma.com.br>
2024-01-11 15:37:58 -0300

    header pronto

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,11 +1,18 @@
 import styles from "./styles.module.css"
+import Link from "next/link"
 const Header =()=>{
     return(
-        <header>
-            <section>
-                <nav>
-                    <h1>Tarefas</h1>
+        <header className={styles.header} >
+            <section className={styles.content}>
+                <nav className={styles.nav}>
+                    <Link href='/'>
+                    <h1 className={styles.logo}>Tarefas <span>+</span></h1>
+                    </Link>
+                    <Link href='/dashboards' className={styles.link}>
+                        Meu Painel
+                    </Link>
                 </nav>
+                <button className={styles.loginButton}>Acessar</button>
             </section>
         </header>
     )
