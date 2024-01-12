6a30617a7e6e1aa31084fd0958dadadd1e425ead
Oscar <oskharm12@gmail.com>
2024-01-11 22:31:00 -0300

    iniciando a criação do login com nextAuth

---
+++ src/pages/dashboards/index.tsx
@@ -0,0 +1,15 @@
+
+import styles from './styles.module.css'
+import Head from 'next/head'
+const  Dashboards =()=>{
+    return(
+        <div className={styles.container}>
+           <Head>
+            <title> Meu Painel</title>
+           </Head>
+           <h1>Pagina Painel</h1>
+        </div>
+    )
+}
+
+export default Dashboards
