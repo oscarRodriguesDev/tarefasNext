3b0cb9d78f91ae2f2498052dcf031f6e5d3c670b
Oscar <oskharm12@gmail.com>
2024-01-13 17:36:34 -0300

    opção de login com conta google finalizada com sucesso

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,21 +1,36 @@
-import styles from "./styles.module.css"
-import Link from "next/link"
-const Header =()=>{
-    return(
-        <header className={styles.header} >
-            <section className={styles.content}>
-                <nav className={styles.nav}>
-                    <Link href='/'>
-                    <h1 className={styles.logo}>Tarefas <span>+</span></h1>
-                    </Link>
-                    <Link href='/dashboards' className={styles.link}>
-                        Meu Painel
-                    </Link>
-                </nav>
-                <button className={styles.loginButton}>Acessar</button>
-            </section>
-        </header>
-    )
-}
+import styles from "./styles.module.css";
+import { useSession, signIn, signOut } from "next-auth/react";
+import Link from "next/link";
 
-export default Header
+const Header = () => {
+  const { data: session, status } = useSession();
+  return (
+    <header className={styles.header}>
+      <section className={styles.content}>
+        <nav className={styles.nav}>
+          <Link href="/">
+            <h1 className={styles.logo}>
+              Tarefas <span>+</span>
+            </h1>
+          </Link>
+          <Link href="/dashboards" className={styles.link}>
+            Meu Painel
+          </Link>
+        </nav>
+        {/*  <button className={styles.loginButton}>Acessar</button> */}
+        {status === "loading" ? (
+          <></>
+        ) : session ? (
+
+          <button className={styles.loginButton} onClick={()=>{signOut()}}>Ola {session?.user?.name}</button>
+        ):(
+            <button className={styles.loginButton} onClick={()=>{signIn()}}>Acessar</button> 
+        )
+        
+        }
+      </section>
+    </header>
+  );
+};
+
+export default Header;
