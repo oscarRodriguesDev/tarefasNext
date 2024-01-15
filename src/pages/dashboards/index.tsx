3b33d064feeaa643042815cd8338f87e697114b8
Dikma Digital2 <dikma.digital2@grupodikma.com.br>
2024-01-15 16:42:18 -0300

    criação do corpo de tarefas

--- src/pages/dashboards/index.tsx
+++ src/pages/dashboards/index.tsx
@@ -2,31 +2,61 @@ import styles from "./styles.module.css";
 import Head from "next/head";
 import { GetServerSideProps } from "next";
 import { getSession } from "next-auth/react";
-import Textarea from '../../components/textarea'
-
+import Textarea from "../../components/textarea";
+import {FiShare2} from 'react-icons/fi'
+import {FaTrash} from 'react-icons/fa'
 const Dashboards = () => {
   return (
     <div className={styles.container}>
       <Head>
         <title> Meu Painel</title>
       </Head>
-     <main className={styles.main}>
+      <main className={styles.main}>
+        {/* essa section é parte superior da minha pagina, onde crio cada uma das tarefas */}
         <section className={styles.content}>
-            <div className={styles.contentForm}>
-                <h1 className="styles.title">Qual sua Tarefa</h1>
-                <Textarea
-                placeholder="Digite sua Tarefa..."
-                />
-                <div className={styles.checkBoxArea}>
-
-                    <input type="checkbox"  className={styles.checkbox}/>
-                    <label>Deixar tarefa publica</label>
-                </div>
-                <button className={styles.button} type='submit'>Registrar</button>
+          <div className={styles.contentForm}>
+            <h1 className={styles.title}>Qual sua Tarefa</h1>
+            <Textarea placeholder="Digite sua Tarefa..." />
+            <div className={styles.checkBoxArea}>
+              <input type="checkbox" className={styles.checkbox} />
+              <label>Deixar tarefa publica</label>
             </div>
+            <button className={styles.button} type="submit">
+              Registrar
+            </button>
+          </div>
         </section>
 
-     </main>
+        
+        {/* já essa section vai ficar na parte inferior onde vai ficar... */}
+        <section className={styles.taskContainer}>
+          <h1>Minhas Tarefas</h1>
+
+          <article className={styles.task}>
+            <div className={styles.tagContainer}>
+              <label className={styles.tag}>Publica</label>
+              <button className={styles.shareButton}>
+              <FiShare2
+              size={22}
+              color='#3183ff'
+              />
+
+              </button>
+             
+            </div>
+
+            <div className={styles.taskContent}>
+              <p>minha primeria tarefa de exemplo</p>
+              <button className={styles.trashButton}>
+              <FaTrash
+              size={24}
+              color='#ea3140'
+              />
+              </button>
+            </div>
+          </article>
+        </section>
+      </main>
     </div>
   );
 };
@@ -34,8 +64,8 @@ const Dashboards = () => {
 export default Dashboards;
 
 /* o metodo abaixo permite que seja possivel privar rotas */
-export const getServerSideProps: GetServerSideProps = async ({req}) => {
-  const session = await getSession( {req} );
+export const getServerSideProps: GetServerSideProps = async ({ req }) => {
+  const session = await getSession({ req });
 
   if (!session?.user) {
     //redireciona para a pagina home
