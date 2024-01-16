4e1babacb02d3f3a2d1c84b3ec7df85a65016346
Dikma Digital2 <dikma.digital2@grupodikma.com.br>
2024-01-16 13:19:42 -0300

    listando tarefas do banco

--- src/pages/dashboards/index.tsx
+++ src/pages/dashboards/index.tsx
@@ -5,18 +5,30 @@ import { getSession } from "next-auth/react";
 import Textarea from "../../components/textarea";
 import {FiShare2} from 'react-icons/fi'
 import {FaTrash} from 'react-icons/fa'
-import {ChangeEvent, FormEvent, useState} from 'react'
+import {ChangeEvent, FormEvent, useState, useEffect} from 'react'
 import{db} from '../../services/firebaseConnection' 
-import {addDoc, collection} from 'firebase/firestore'
+import {addDoc, collection, query, orderBy,onSnapshot,where} from 'firebase/firestore'
 
+//interface para configuração da tipagem do user
 interface HomeProps{
   user:{
     email:string
   }
 }
+
+interface taskProps{
+  id:string;
+  created:Date;
+  public:boolean;
+  tarefa:string;
+  user:string;
+
+}
+
 const Dashboards = ({user}:HomeProps) => {
 
 const [input, setInput] =  useState('')
+const [tasks,setTasks] = useState<taskProps[]>([])
 const [publicTask, setPublicTask] =  useState(false)
 
 //função para capturar se o usuario colocou a tarefa como publica ou não
@@ -24,6 +36,33 @@ function handleChangePublic(event:ChangeEvent<HTMLInputElement>){
   console.log(event.target.checked)
  setPublicTask(event.target.checked)
 }
+
+useEffect(()=>{
+async function loadingTarefas(){
+const tarefasRef  = collection (db, 'tarefas')
+const q  = query(
+  tarefasRef,
+  orderBy("created", 'desc'),
+  where('user', '==', user?.email)
+)
+onSnapshot(q,(snapshot) =>{
+  let lista =  []as taskProps[]
+snapshot.forEach((doc)=>{
+    lista.push({
+      id:doc.id,
+      tarefa:doc.data().tarefa,
+      created:doc.data().created,
+      user:doc.data().user,
+      public:doc.data().public
+
+    })
+}) 
+setTasks(lista)
+})
+}
+loadingTarefas();
+},[user?.email])
+
 
 //função para registrar as tarefas
 async function handleRegisterTask(event:FormEvent<HTMLFormElement>){
@@ -87,30 +126,31 @@ async function handleRegisterTask(event:FormEvent<HTMLFormElement>){
         {/* já essa section vai ficar na parte inferior onde vai ficar... */}
         <section className={styles.taskContainer}>
           <h1>Minhas Tarefas</h1>
+       {tasks.map((item)=>(
+         <article key={item.id} className={styles.task}>
+        {item.public&& (
+           <div className={styles.tagContainer}>
+           <label className={styles.tag}>Publica</label>
+           <button className={styles.shareButton}>
+           <FiShare2
+           size={22}
+           color='#3183ff'
+           />
+           </button>
+         </div>
+        )}
 
-          <article className={styles.task}>
-            <div className={styles.tagContainer}>
-              <label className={styles.tag}>Publica</label>
-              <button className={styles.shareButton}>
-              <FiShare2
-              size={22}
-              color='#3183ff'
-              />
-
-              </button>
-             
-            </div>
-
-            <div className={styles.taskContent}>
-              <p>minha primeria tarefa de exemplo</p>
-              <button className={styles.trashButton}>
-              <FaTrash
-              size={24}
-              color='#ea3140'
-              />
-              </button>
-            </div>
-          </article>
+         <div className={styles.taskContent}>
+           <p>{item.tarefa}</p>
+           <button className={styles.trashButton}>
+           <FaTrash
+           size={24}
+           color='#ea3140'
+           />
+           </button>
+         </div>
+       </article>
+       ))}
         </section>
       </main>
     </div>
